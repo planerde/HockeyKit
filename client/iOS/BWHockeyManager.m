2793a259d48bb091459757e32d9c92a7aa4a3a4c
Tim Preuß <tim.preuss@dmgmori.com>
2015-07-22 10:48:18 +0200

    exit the app after the system opened the installation link

--- client/iOS/BWHockeyManager.m
+++ client/iOS/BWHockeyManager.m
@@ -913,6 +913,11 @@ static NSString *kHockeyErrorDomain = @"HockeyErrorDomain";
   BWHockeyLog(@"API Server Call: %@, calling iOS with %@", hockeyAPIURL, iOSUpdateURL);
   BOOL success = [[UIApplication sharedApplication] openURL:[NSURL URLWithString:iOSUpdateURL]];
   BWHockeyLog(@"System returned: %d", success);
+    
+  // timpreuss: Force the app to close
+  if (success)
+    exit(0);
+    
   return success;
 }
 
